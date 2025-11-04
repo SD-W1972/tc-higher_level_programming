46bd77666b4db45098b913eb174da177533d049d
SD-W1972 <anthonybemol@gmail.com>
2025-11-04 12:05:28 +0000

    feat: last digit avalation program in python

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = int(str(number)[-1])
+if number < 0:
+    last_digit = -last_digit
+
+if last_digit > 5:
+    str_msg = "greater than 5"
+elif last_digit == 0:
+    str_msg = "0"
+else:
+    str_msg = "less than 6 and not 0"
+print(f"Last digit of {number} is {last_digit} and is {str_msg}")

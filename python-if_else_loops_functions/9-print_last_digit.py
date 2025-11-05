97d3ab3795065a2d44291770b229ee9118a7914a
SD-W1972 <you@example.com>
2025-11-05 10:44:19 +0000

    feat: adding negative number validation

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(number % 10, end="")
-    return number % 10
+    last_digit = 0
+    if(number > 0):
+        last_digit = number % 10
+    else:
+        last_digit = number * -1 % 10
+    print(last_digit)
+    return last_digit

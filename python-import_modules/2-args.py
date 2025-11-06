5930441fe718f4ec11b0346b69f7134e917320f3
SD-W1972 <anthonybemol@gmail.com>
2025-11-06 10:51:17 +0000

    feat: once again, minor changes

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     elif argc == 1:
         print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments".format(argc))
+        print("{} arguments:".format(argc))
         for i in range (1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))

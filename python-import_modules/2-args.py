f8f979aa47935e46b9304f8283c9862dec3dfc9e
SD-W1972 <anthonybemol@gmail.com>
2025-11-06 10:50:38 +0000

    feat: minor changes

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
-        print("1 argument:\n{}".format(sys.argv[1]))
+        print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
         print("{} arguments".format(argc))
         for i in range (1, len(sys.argv)):

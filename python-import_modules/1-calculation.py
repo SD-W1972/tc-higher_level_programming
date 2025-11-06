7e60fdeddd7818df03e9a8b29a0628587faa636a
SD-W1972 <anthonybemol@gmail.com>
2025-11-06 10:23:37 +0000

    feat: importing the calculator

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1 import *
+
+a = 10
+b = 5
+add = add(a, b)
+sub = sub(a, b)
+mul = mul(a, b)
+div = div(a, b)
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add))
+    print("{} - {} = {}".format(a, b, sub))
+    print("{} * {} = {}".format(a, b, mul))
+    print("{} / {} = {}".format(a, b, div))
+     

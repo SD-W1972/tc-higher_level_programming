574db6d764b12757856a45d4ed8a82158e88b523
SD-W1972 <anthonybemol@gmail.com>
2025-11-07 11:46:48 +0000

    feat: test

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from calculator_1 import *
 import sys
+if __name__ != "__main__":
+    sys.exit(0)
 
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")

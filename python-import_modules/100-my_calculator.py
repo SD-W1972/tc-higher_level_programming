cf113b4ee13358c42404bb045e917c3b0db3d91d
SD-W1972 <anthonybemol@gmail.com>
2025-11-07 11:43:21 +0000

    feat: interactive calculator

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from calculator_1 import *
+import sys
+
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+
+
+if sys.argv[2] == "+":
+    print("{} + {} = {}".format(a, b, (a+b)))
+elif sys.argv[2] == "-":
+    print("{} - {} = {}".format(a, b, (a-b)))
+elif sys.argv[2] == "*":
+    print("{} * {} = {}".format(a, b, (a*b)))
+elif sys.argv[2] == "/":
+    print("{} / {} = {}".format(a, b, (a/b)))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+    

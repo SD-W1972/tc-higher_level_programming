e9f349a6db8cb55389b5bc11e69533e2157ca2ce
SD-W1972 <anthonybemol@gmail.com>
2025-11-10 13:05:35 +0000

    feat: typo

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def multiple_returns(frase):
-    leng = len(frase)
-    car = frase[0] if l > 0 else None
-    tuple = (leng, car)
-    return tuple
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else None
+    return (length, first_char)

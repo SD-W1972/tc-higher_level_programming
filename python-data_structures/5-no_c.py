c9696694f598aae89fe7cd61ff716c1464bde773
SD-W1972 <anthonybemol@gmail.com>
2025-11-10 12:04:59 +0000

    feat: once again, pycodestyles

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_str = ""
     for char in my_string:
         if char != "c" and char != "C":
-            new_str += char       
+            new_str += char
     return new_str

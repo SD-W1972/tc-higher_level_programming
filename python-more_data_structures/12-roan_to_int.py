ccf6fc6d37284a29a6fa0dd8a340cdce572a3bb8
SD-W1972 <anthonybemol@gmail.com>
2025-11-14 12:57:55 +0000

    feat: interview question lesgooo

---
+++ python-more_data_structures/12-roan_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        current_value = roman[char]
+
+        if current_value < prev_value:
+            total -= current_value
+        else:
+            total += current_value
+
+        prev_value = current_value
+    return total

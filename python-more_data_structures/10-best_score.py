99135af97d630fe62245f0c7db8f0f053572b3d7
SD-W1972 <anthonybemol@gmail.com>
2025-11-14 12:14:37 +0000

    feat: small typo

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     biggest = 0
     biggest_key = None
-    for key,value in a_dictionary.items:
+    for key,value in a_dictionary.items():
         if value > biggest:
             biggest = a_dictionary.get(key)
             biggest_key = key

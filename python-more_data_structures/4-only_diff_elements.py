f4f8f139ddfb6f2ab4f846eb06ec314f229b95fa
SD-W1972 <anthonybemol@gmail.com>
2025-11-17 09:58:59 +0000

    feat: now its gotta work

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1 & set_2
+    control = False
+    set_3 = set()
+
+    for item in set_1:
+        if item in set_2:
+            control = False
+        else:
+            set_3.add(item)
+
+    for item in set_2:
+        if item in set_1:
+            control = False
+        else:
+            set_3.add(item)
+
+    return set_3

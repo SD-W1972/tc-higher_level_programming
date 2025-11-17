45a5c5be6dd7c8d65354a3501c8110e3c2524dfe
SD-W1972 <anthonybemol@gmail.com>
2025-11-17 12:02:08 +0000

    feat: actually a much simpler way to calc the weighted average

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-from functools import reduce
-
 def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    sum_values_weights = reduce(lambda acc, item: acc + (item[0] * item[1]), my_list, 0)
-    sum_weights = reduce(lambda acc, item: acc + item[1], my_list, 0)
+    sum_values_weights = 0
+    sum_weights = 0
+
+    for score, weight in my_list:
+        sum_values_weights += score * weight
+        sum_weights += weight
+
     return sum_values_weights / sum_weights

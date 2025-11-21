4dc10adab7c94f3bac1ee7d7712cf7df49509254
SD-W1972 <anthonybemol@gmail.com>
2025-11-21 15:35:47 +0000

    feat: output correction

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,6 +2,6 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
-            print("nb_print: {:d}".format(my_list[i]))
+            print(my_list[i], end="")
     except IndexError as ex:
         print("índice fornecido é maior do que o tamanho da lista fornecida")

e454ae44d6ee0344d0a11c1baf02d309329406f1
SD-W1972 <anthonybemol@gmail.com>
2025-11-21 15:39:01 +0000

    feat: minor changes

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(0, x):
             print(my_list[i], end="")
+            count += 1
     except IndexError as ex:
-        print("índice fornecido é maior do que o tamanho da lista fornecida")
+        pass
+    print()
+    return count

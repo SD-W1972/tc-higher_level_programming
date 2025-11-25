f6d3d5853fb9451656c88d7257a301018538c3bb
SD-W1972 <anthonybemol@gmail.com>
2025-11-25 12:52:37 +0000

    feat: getting rid of bullshit validation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -26,8 +26,8 @@ def print_square(size):
         raise(TypeError("size must be an integer"))
     if size < 0: 
         raise(ValueError("size must be >= 0"))
-    if isinstance(size, float) and size <= 0: 
-        raise(TypeError("size must be an integer"))
+#    if isinstance(size, float) and size <= 0: 
+ #       raise(TypeError("size must be an integer"))
 
     for i in range(size):
         for j in range(size):

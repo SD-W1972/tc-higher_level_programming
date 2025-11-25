95714a6de19111e222e2cc3d8a13814860ccbfe2
SD-W1972 <anthonybemol@gmail.com>
2025-11-25 23:15:33 +0000

    feat: minor changes

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
 """
-Text identation module
-This module provides a function tha identates the 
-provided string on its conducts
+Text indentation module
+This module provides a function that indents the 
+provided string on specific characters
 """
 
 
 def text_indentation(text):
     """
-    Prints the provided string and identates it by
-    breaking a line if it finds a character that's equal
-    to "," ":" or "?"
+    Prints the provided string and indents it by
+    adding two new lines when finding '.', ':' or '?'
 
     Args:
         text: string
-    
+
     Raises:
         TypeError: if text is not a str object
 
     Returns:
         Nothing
     """
-
     if not isinstance(text, str):
-        raise(TypeError("text must be a string"))
+        raise TypeError("text must be a string")
 
     text = text.strip()
-    for i in range(0, len(text)):
+    i = 0
+    while i < len(text):
         print(text[i], end="")
-        if text[i] == "." or text[i] == "?" or text[i] == ":":
-            print("\n\n" ,end="")
+        if text[i] in ['.', '?', ':']:
+            print("\n\n", end="")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1

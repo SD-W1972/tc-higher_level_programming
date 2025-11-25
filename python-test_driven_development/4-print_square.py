95200b0630e1a812679aa9e7f1669c43788f8d68
SD-W1972 <anthonybemol@gmail.com>
2025-11-25 12:36:32 +0000

    feat basic implementation and documentation to print square module

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Print square module
+This module provides a function to
+print a square with the desired number
+of #
+"""
+
+def print_square(size):
+    """ 
+    Prints a square with x number of # in its formation
+
+    Args:
+        size: the desired size to your square(integer or float)
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is < than 0
+        TypeError: if size is a float and < than 0
+    Returns:
+        Nothing
+    """
+
+    if not isinstance(size, int): 
+        raise(TypeError("size must be an integer"))
+    if size < 0: 
+        raise(ValueError("size must be >= 0"))
+    if isinstance(size, float) and size < 0: 
+        raise(TypeError("size must be an integer"))
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()

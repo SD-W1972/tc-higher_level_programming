ef2d081386cdd10e160813f8510002cd077cd59c
SD-W1972 <anthonybemol@gmail.com>
2025-11-25 12:56:23 +0000

    feat: pep8 validation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -8,26 +8,24 @@ of #
 
 
 def print_square(size):
-    """ 
+    """
     Prints a square with x number of # in its formation
 
     Args:
-        size: the desired size to your square(integer or float)
+        size: the desired size to your square(integer)
 
     Raises:
         TypeError: if size is not an integer
         ValueError: if size is < than 0
-        TypeError: if size is a float and < than 0
+
     Returns:
         Nothing
     """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
 
-    if not isinstance(size, int): 
-        raise(TypeError("size must be an integer"))
-    if size < 0: 
-        raise(ValueError("size must be >= 0"))
-#    if isinstance(size, float) and size <= 0: 
- #       raise(TypeError("size must be an integer"))
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
     for i in range(size):
         for j in range(size):

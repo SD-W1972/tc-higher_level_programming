9ea8c9573be7a9d124519bbec10975007beedc9b
SD-W1972 <anhtonybemol@gmail.com>
2025-11-26 11:12:03 +0000

    feat: pep8 validation

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -28,45 +28,45 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    
+
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-    
+
     if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
         raise ValueError("m_a can't be empty")
     if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
         raise ValueError("m_b can't be empty")
-    
+
     for row in m_a:
         for element in row:
             if not isinstance(element, (float, int)):
                 raise TypeError("m_a should contain only integers or floats")
-    
+
     for row in m_b:
         for element in row:
             if not isinstance(element, (float, int)):
                 raise TypeError("m_b should contain only integers or floats")
-    
+
     a_cols = len(m_a[0])
     for row in m_a:
         if len(row) != a_cols:
             raise TypeError("each row of m_a must be of the same size")
-    
+
     b_cols = len(m_b[0])
     for row in m_b:
         if len(row) != b_cols:
             raise TypeError("each row of m_b must be of the same size")
-    
+
     if a_cols != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    
+
     result = [[0 for _ in range(b_cols)] for _ in range(len(m_a))]
 
     for i in range(len(m_a)):
         for j in range(b_cols):
             for k in range(len(m_b)):
                 result[i][j] += m_a[i][k] * m_b[k][j]
-    
+
     return result

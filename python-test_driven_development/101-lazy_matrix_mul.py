bf12dafbad0355194b1bc5d43baa62f17b08430f
SD-W1972 <anthonybemol@gmail.com>
2025-11-27 10:09:23 +0000

    feat: this gotta work

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
 Lazy matrix multiplication module
-This module provides a function to
-multiply two matrices using NumPy
+This module provides a function to multiply two matrices using NumPy
 """
 
 import numpy as np
@@ -18,61 +17,11 @@ def lazy_matrix_mul(m_a, m_b):
 
     Returns:
         Result of matrix multiplication as a NumPy array
-
-    Raises:
-        TypeError: if m_a or m_b aren't lists
-        TypeError: if m_a or m_b aren't matrices  
-        ValueError: if m_a or m_b are empty
-        TypeError: if m_a or m_b contains numbers 
-        that aren't floats or integers
-        TypeError: if the number of rows isn't even
-        on m_a or m_b
-        ValueError: if m_a and m_b can't be multiplied
     """
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-
-    if not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-
-    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
-        raise ValueError("m_a can't be empty")
-    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
-        raise ValueError("m_b can't be empty")
-
-    for row in m_a:
-        for element in row:
-            if not isinstance(element, (float, int)):
-                raise TypeError
-                ("m_a should contain only integers or floats")
-
-    for row in m_b:
-        for element in row:
-            if not isinstance(element, (float, int)):
-                raise TypeError
-                ("m_b should contain only integers or floats")
-
-    a_cols = len(m_a[0])
-    for row in m_a:
-        if len(row) != a_cols:
-            raise TypeError
-            ("each row of m_a must be of the same size")
-
-    b_cols = len(m_b[0])
-    for row in m_b:
-        if len(row) != b_cols:
-            raise TypeError
-            ("each row of m_b must be of the same size")
-
-    if a_cols != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-
     try:
-        result = np.matmul(np.array(m_a), np.array(m_b))
+        result = np.matmul(m_a, m_b)
         return result
-    except Exception:
-        raise ValueError("m_a and m_b can't be multiplied")
+    except ValueError as e:
+        raise ValueError(str(e)) from e
+    except Exception as e:
+        raise

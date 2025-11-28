a7e439e8d4924533ebc28239992e820b3441773a
SD-W1972 <anthonybemol@gmail.com>
2025-11-28 09:54:43 +0000

    feat: add documentation to square module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,2 +1,18 @@
+#!/usr/bin/python3
+"""
+Square class module
+This module defines an empty Square class
+"""
+
+
 class Square:
+    """
+    An empty class that defines a square
+
+    This class currently doesn't have any attributes or methods.
+    It serves as a basic blueprint for square objects.
+
+    Attributes:
+        None currently
+    """
     pass

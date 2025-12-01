735a53be2f7b92a03d85073a2d5436e12967ba93
SD-W1972 <anthonybemol@gmail.com>
2025-12-01 22:13:02 +0000

    feat: add empty rectangle module

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Rectangle class module
+This module defines an empty Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    An empty class that defines a rectanle
+
+    This class currently doesn't have any attributes or functions
+    It servers as a basic blueprint for rectangles
+
+    Attributes:
+        None
+    """
+    pass

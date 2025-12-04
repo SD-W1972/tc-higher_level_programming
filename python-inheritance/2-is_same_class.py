9b36628d3cfbc88b98eecc0828751c7b33653fde
SD-W1972 <anthonybemol@gmail.com>
2025-12-04 12:26:10 +0000

    feat: testing a new logic on same class module

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -9,7 +9,4 @@ object is an instance from a provided class
 def is_same_class(obj, a_class):
     """Returns True if obj is an instance from a_class,
     returns False if it's not"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class

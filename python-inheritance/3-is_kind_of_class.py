84421529f44d45b1c7a5df657d5f2b5a12469275
SD-W1972 <anthonybemol@gmail.com>
2025-12-04 12:42:28 +0000

    fix: pep8 validation

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Is kind of class module
-This module provides a method tha verifies if
-a provided obj is an istance of a provided class
-or if, its an instance of a class that inherited
-from the provided class
+Check if object is instance of class or inherited class
+This module provides a function to check if an object
+is an instance of, or inherits from, a specified class
 """
+
+
 def is_kind_of_class(obj, a_class):
     """Returns True if it's an instance, False if
     it's not"""

2801638c36d416e99d4c586816007cca956ed546
SD-W1972 <anthonybemol@gmail.com>
2025-12-05 10:34:32 +0000

    feat: add an extra method to the base geometry class

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Base geometry module
+This class provides a non implemented area
+method
+"""
+
+
+class BaseGeometry:
+    """
+    Base Geometry class
+    """
+    def area(self):
+        """
+        Area method
+
+        Raises an Exception
+        """
+        raise Exception("area() is not implemented")
+    def integer_validation(self, name, value):
+        """This method validates the value attribute"""
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")

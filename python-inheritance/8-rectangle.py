161705f1ad18ea24ac70cf60463cc9417a856ca4
SD-W1972 <anthonybemol@gmail.com>
2025-12-05 12:07:11 +0000

    feat: using inheritance with base geometry and rectangle modules

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rectangle class inheriting from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a rectangle with width and height"""
+
+    def __init__(self, width, height):
+        """Initialize rectangle with validated width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height

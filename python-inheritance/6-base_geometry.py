235cc482d0ca5282e12a13b0173fd74f850e8a57
SD-W1972 <anthonybemol@gmail.com>
2025-12-05 10:28:20 +0000

    feat: add non implemented method area() to base geometry class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Base geometry module
+This class provides a non implemented area
+method
+"""
+
+
+class BaseGeometry:
+    """
+    Base Geometry class
+    """
+    def area(self):
+        """
+        Area method
+
+        Raises an Exception
+        """
+        raise(Exception("area() is not implemented"))

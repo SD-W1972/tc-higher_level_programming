2b11d3c63f3f86abffd309612ba27d5d10dfcd89
SD-W1972 <anthonybemol@gmail.com>
2025-12-08 10:23:21 +0000

    feat: implmement square module inheriting from rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class inheriting from Rectangle"""
+BaseGeometry = __import__('9-rectangle').Rectangle
+
+
+class Rectangle(Rectangle):
+    """Defines a square with size"""
+
+    def __init__(self, size):
+        self.integer_validator(size)
+        self.__size = size
+
+    def area(self):
+        """Return square area"""
+        return self.__size * self.__size

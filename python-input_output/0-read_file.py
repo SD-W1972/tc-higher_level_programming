d732b146841a9af3659c5909b183f1e2469ada8a
SD-W1972 <anthonybemol@gmail.com>
2025-12-12 11:31:31 +0000

    feat: add docstring to read file module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+""" Read file module """
+
+
 def read_file(filename=""):
-    
+    """
+    Read file function
+
+    Prints the content of the provided
+    file, otherwise raises an exception
+ 
+    Args:
+        filename: string
+
+    Returns:
+        Nothing
+    """    
     try:
         with open(filename, 'r', encoding='utf-8')as file:
             content = file.read()

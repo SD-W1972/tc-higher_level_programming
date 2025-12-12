5118d46949aab0f9be96db5424ca84b80bc2ac99
SD-W1972 <anthonybemol@gmail.com>
2025-12-12 12:05:49 +0000

    feat: add to json string module

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" String to json module """
+
+
+def to_json_string(my_obj):
+    """
+    to_json_string function
+
+    Args:
+        my_obj (string)
+
+    Returns:
+        json object of my_obj
+    """
+    return json.dumps(my_obj)

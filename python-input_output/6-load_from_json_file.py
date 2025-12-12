6f26b5ef1f2f58767769ef7551f7b5558e3d9161
SD-W1972 <anthonybemol@gmail.com>
2025-12-12 12:35:38 +0000

    testing smth out

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,14 +4,6 @@ import json
 
 
 def load_from_json_file(filename):
-    """
-    Load object from JSON file
-
-    Args:
-        filename (str): Name of JSON file
-
-    Returns:
-        object: Python object loaded from JSON
-    """
+    """Load object from JSON file (creates but doesn't return)"""
     with open(filename, 'r', encoding='utf-8') as file:
-        return json.load(file)
+        data = json.load(file)

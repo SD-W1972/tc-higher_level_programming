21b280cda7e3d555dc3d00758fd057e73906d357
SD-W1972 <anthonybemol@gmail.com>
2025-12-12 12:15:17 +0000

    feat: impklement from json string to object module

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Json string to object module"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    from_json_string function
+
+    Args:
+        my_str (string)
+
+    Returns: 
+        Json obj
+    """
+    return json.loads(my_str)

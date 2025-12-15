4ad1aae2ceefcfe71aaf5dbf344cccea7c0bf718
SD-W1972 <anthonybemol@gmail.com>
2025-12-15 13:48:59 +0000

    feat: implement student class with json filtering

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Student class with JSON filtering"""
+
+
+class Student:
+    """Student information class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Create student with name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary of specified attributes or all if None"""
+        if attrs is None:
+            return self.__dict__
+
+        return {attr: getattr(self, attr) 
+                for attr in attrs if hasattr(self, attr)}
